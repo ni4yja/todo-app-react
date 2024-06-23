5467005bb199e8c088db8f8eedccc68039b88152
Liuba Kuibida <k2.liuba@gmail.com>
2024-06-23 16:20:22 +0200

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: false,
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,59 @@
-import "./App.css";
-import { useState } from "react";
+import './App.css'
+import { useState } from 'react'
 
 function App() {
   const defaultData = [
     {
       done: false,
-      content: "Write a blog post",
-      type: "Personal",
+      content: 'Write a blog post',
+      type: 'Personal',
       id: Date.now(),
     },
-  ];
+  ]
 
-  const [todos, setTodos] = useState(defaultData);
+  const [todos, setTodos] = useState(defaultData)
 
   function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const query = formData.get("query");
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const query = formData.get('query')
     setTodos([
       ...todos,
       {
         done: false,
         content: query,
-        type: "Personal",
+        type: 'Personal',
         id: Date.now(),
       },
-    ]);
-    event.target.reset();
+    ])
+    event.target.reset()
   }
 
-  const todoItemsList = todos.map((todo) => (
-    <li key={todo.id} className='todo-item'>
+  const todoItemsList = todos.map(todo => (
+    <li key={todo.id} className="todo-item">
       {todo.content}
     </li>
-  ));
+  ))
 
   return (
-    <div className='app-wrapper'>
-      <section className='add-container'>
+    <div className="app-wrapper">
+      <section className="add-container">
         <h2>New Task</h2>
         <form onSubmit={handleSubmit}>
-          <label htmlFor='newTodo'>
-            <input type='text' name='query' className='new-todo' id='newTodo' />
+          <label htmlFor="newTodo">
+            <input type="text" name="query" className="new-todo" id="newTodo" />
           </label>
-          <button type='submit' className='add-btn'>
+          <button type="submit" className="add-btn">
             Add task
           </button>
         </form>
       </section>
-      <section className='review-container'>
+      <section className="review-container">
         <h2>ToDo List</h2>
-        <ul className='todo-list'>{todoItemsList}</ul>
+        <ul className="todo-list">{todoItemsList}</ul>
       </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
